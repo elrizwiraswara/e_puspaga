4dc3a8c79e835d79d61ab6849d6dffaffb1c67e6
Elriz Wiraswara <elrizwiraswara@gmail.com>
2024-10-19 21:08:11 +0700

    update splash view

--- lib/view/splash/splash_view.dart
+++ lib/view/splash/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:app_image/app_image.dart';
 import 'package:flutter/material.dart';
 
+import '../../app/services/firebase/auth/auth_service.dart';
+import '../../app/services/locator/locator.dart';
 import '../../app/themes/app_assets.dart';
 
 class SplashView extends StatefulWidget {
@@ -13,6 +15,16 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final navigator = Navigator.of(context);
+      final auth = locator<AuthService>();
+      auth.checkIsSignedIn(navigator);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
